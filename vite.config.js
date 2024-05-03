3e715a03b4bd29f3a052057b37d05215f332b17e
xYOKI <131733092+xYOKI@users.noreply.github.com>
2024-05-03 22:55:57 +0800

    modify the base path and output  directory

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const SITE_PATH = "album";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  base: SITE_PATH,
+  build: {
+    outDir: SITE_PATH,
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+});
