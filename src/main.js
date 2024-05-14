2fe88df33fb2629f1adf8857fafd2a65a4a82738
xYOKI <131733092+xYOKI@users.noreply.github.com>
2024-05-14 22:50:07 +0800

    Realize waterfall effect in images

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
+// import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import V3waterfall from 'v3-waterfall'
+import 'v3-waterfall/dist/style.css'
 
-createApp(App).mount('#app')
+
+createApp(App).use(V3waterfall).mount('#app')
