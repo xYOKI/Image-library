a044043b43a3f17b96e36ec7fef4a5fc9297e2c5
xYOKI <131733092+xYOKI@users.noreply.github.com>
2024-05-15 00:30:54 +0800

    New login page

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,13 @@ import V3waterfall from 'v3-waterfall'
 import 'v3-waterfall/dist/style.css'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(V3waterfall)
+app.use(ElementPlus)
 app.mount('#app')
---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-form :model="form" label-width="auto" style="max-width: 600px">
+    <el-form-item label="Activity name">
+      <el-input v-model="form.name" />
+    </el-form-item>
+    <!-- password-->
+    <el-form-item label="Password">
+      <el-input v-model="form.password" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">Login</el-button>
+    </el-form-item>
+  </el-form>
+</template>
+
+<script lang="ts" setup>
+import { reactive } from 'vue'
+
+// do not use same name with ref
+const form = reactive({
+  name: '',
+  password: ''
+})
+
+const onSubmit = () => {
+  console.log('submit!', form)
+}
+</script>
