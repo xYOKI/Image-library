82dfe579b83db8d4d698121d98ea955ea0585ecb
xYOKI <131733092+xYOKI@users.noreply.github.com>
2024-05-14 23:33:01 +0800

    initial vue ts project

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
-<script setup>
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
 import WaterfallFlow from "./components/WaterfallFlow.vue";
 </script>
 
 <template>
   <waterfall-flow />
+  <RouterView />
 </template>
 
 <style scoped>
